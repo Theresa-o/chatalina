3d238d1e85115a63eee4caa096f8518f86a1e8aa
Theresa-o <62052443+Theresa-o@users.noreply.github.com>
2024-01-01 22:04:18 +0100

    Update the login page form with validation

--- src/components/Auth/Login/Login.tsx
+++ src/components/Auth/Login/Login.tsx
@@ -1,4 +1,16 @@
+import { useForm } from "react-hook-form";
+
+interface formData {
+    email: string
+    password: string
+}
+
 const Login = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm<formData>();
+
+    const onSubmit = handleSubmit(({email, password}) => {
+        console.log(email, password);
+    })
   return (
     <>
     <div className='min-h-screen bg-gray-50 flex flex-col justify-center'>
@@ -7,14 +19,17 @@ const Login = () => {
             <div className='text-3xl font-bold text-gray-300 mt-2 text-center'>another text</div>
         </div>
         <div className='max-w-md w-full mx-auto mt-4 bg-white p-8 border border-gray-300'>
-            <form action='' className='space-y-6'>
+            <form action='' className='space-y-6' onSubmit={onSubmit}>
                 <div>
                     <label htmlFor='' className='text-sm font-bold text-gray-600 block'>Email</label>
-                    <input type='text' className='w-full p-2 border border-gray-300 rounded mt-1'></input>
+                    <input {...register('email', {required: true, minLength: 10})} name="email" type='text' className='w-full p-2 border border-gray-300 rounded mt-1'/>
+                    {errors.email && <span>Invalid details</span>}
+
                 </div>
                 <div>
                     <label htmlFor='' className='text-sm font-bold text-gray-600 block'>Password</label>
-                    <input type='text' className='w-full p-2 border border-gray-300 rounded mt-1'></input>
+                    <input {...register('password', {required: true, minLength: 8})} name="password" type='text' className='w-full p-2 border border-gray-300 rounded mt-1'/>
+                    {errors.password && <span>Invalid details</span>}
                 </div>
                 <div><a href="#" className="font-medium text-sm text-blue-500">Forgot Password?</a></div>
                 <div>
